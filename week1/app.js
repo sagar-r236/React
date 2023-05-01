66209a25efb32a929f23cdecc7b9fa0959696a6b
sagarm2s <sagar@m2s.co.in>
2023-05-01 07:42:58 +0530

    add app.js index.css

---
+++ week1/app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "header",
+    className: "head",
+  },
+  "Hello World!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
