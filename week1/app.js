3347f5adf62e1e777e456c61bd2d4972c43cfa7b
sagar-r236 <sagar236@protonmail.com>
2023-05-20 21:27:00 +0530

    JSX finished

--- week1/app.js
+++ week1/app.js
@@ -2,16 +2,45 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 
 
-
 const heading = React.createElement(
   "h1",
   {
     id: "header",
     className: "head",
+    key: 1,
   },
   "Hello World! from parcel"
 );
 
+const heading2 = React.createElement(
+  "h2",
+  {
+    className: "head",
+    key: 2
+  },
+  "heading 2"
+)
+
+const heading3 = (
+  <h1 id="heading3" className="head" key="3">Heading3 inside jsx</h1>
+)
+
+const container = React.createElement(
+  "div",
+  {
+    id : "container"  
+  }, 
+  [heading, heading2, heading3]
+)
+
+const HeaderComponent = () => {
+  return <h1>Hello from header component</h1>
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log('check')
-root.render(heading);
+root.render(<HeaderComponent />);
+
+
